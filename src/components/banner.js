bdfa764cacc7e104dedde6ef249fb20a751bbba9
Stephen Parker <sparker888@gmail.com>
2024-06-17 19:17:30 -0400

    update banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -46,7 +46,7 @@ export default function Banner() {
           target="_blank"
           className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
-          Read now <span aria-hidden="true">&rarr;</span>
+          Read the case study <span aria-hidden="true">&rarr;</span>
         </a>
       </div>
       <div className="flex flex-1 justify-end">
