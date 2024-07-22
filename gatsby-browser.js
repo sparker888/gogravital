7b1306cbca868bf6efa4ff8fac966c8fa83acd9a
Stephen Parker <sparker888@gmail.com>
2024-07-22 17:19:51 -0400

    update gatsby-browser and gatsby-ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,1 @@
 import './src/styles/global.css'
-import React from "react"
-
-export const onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script
-      key="plausible"
-      async
-      defer
-      data-domain="gravitaldigital.com"
-      src="https://plausible.io/js/plausible.js"
-    ></script>,
-  ])
-}
